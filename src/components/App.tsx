c06e0f189025ed53ed69ac1f844430a53e50b9c8
Zhanadil <zhanadil1509@gmail.com>
2019-08-31 06:15:10 +0600

    all files

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './index.scss'
+
+class App extends React.Component<any, any> {
+  render() {
+    return (
+      <>
+        <h1 className='test'>Welcome to React with Typescript</h1>
+      </>
+    );
+  }
+}
+
+export default App;
